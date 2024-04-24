3b5a5ceb45602f63ccd53ad8874ca70d3d4d3fd7
firoj-mahamud <mdfirojmahamud514@gmail.com>
2024-04-24 22:38:11 +0600

    BMi Splash Screen Done

---
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.bmi
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,bmi::class.java))
+        },300)
+
+    }
+}
